b8752fe2a685ca6a9ed00783260337767f34f225
Isala Vineth <isalavineth@gmail.com>
2025-05-29 20:03:38 +0530

    Delete OOP_Assignment directory

--- OOP_Assignment/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Create and set up the main frame
-        MyFrame frame = new MyFrame();
-        frame.setVisible(true);
-    }
-}
